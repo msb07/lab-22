6be31766f6f8916ba617199c1b5c5a3eb8d47b0f
Mauricio Silva <mauriciospta@gmail.com>
2022-04-12 12:52:07 -0300

    criacao de hook para gerenciar o estado do carrinho e a quantidade de produtos

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,49 @@
+import create from 'zustand';
+import { ProductProps } from '../components/Product';
+
+type CartProducts = {
+  cart: ProductProps[];
+  IncreaseAmount: (product: ProductProps) => void;
+  decreaseAmount: (product: ProductProps) => void;
+};
+
+export const useCart = create<CartProducts>((set) => ({
+  cart: [],
+
+  IncreaseAmount: (product: ProductProps) => {
+    set(({ cart }) => {
+      const isProductInCart = cart.find((item) => item.id === product.id);
+
+      if (isProductInCart) {
+        return {
+          cart: cart.map((item) =>
+            item.id === product.id && item.quantity > item.amount
+              ? { ...item, amount: item.amount + 1 }
+              : item
+          ),
+        };
+      }
+      return {
+        cart: [...cart, { ...product, amount: 1 }],
+      };
+    });
+  },
+  decreaseAmount: (product: ProductProps) => {
+    set(({ cart }) => {
+      const isProductInCart = cart.find((item) => item.id === product.id);
+      if (isProductInCart) {
+        //Se estiver, soma a quantidade
+        return {
+          cart: cart.map((item) =>
+            item.id === product.id && item.amount > 0
+              ? { ...item, amount: item.amount - 1 }
+              : item
+          ),
+        };
+      }
+      return {
+        cart: [...cart, { ...product, amount: 0 }],
+      };
+    });
+  },
+}));
